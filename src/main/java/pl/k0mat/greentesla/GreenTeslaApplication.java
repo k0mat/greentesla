2691ac3d0cb8c582d393d0d530c2dcb49885282a
k0mat <kow.mateusz@gmail.com>
2023-03-27 17:33:07 +0200

    Init spring boot (#2)

---
+++ src/main/java/pl/k0mat/greentesla/GreenTeslaApplication.java
@@ -0,0 +1,13 @@
+package pl.k0mat.greentesla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTeslaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenTeslaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/k0mat/greentesla/GreenTeslaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.k0mat.greentesla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenTeslaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
